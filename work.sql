6b71d05d20a70c05f6fee23b7cdc1020e07eeb9b
koubaib <koubaibbenyedder@gmail.com>
2022-06-14 15:41:21 +0000

    Data Manipulation language (DML) Checkpoint

---
+++ work.sql
@@ -0,0 +1,11 @@
+INSERT INTO Customer (Customer_id,customer_Name,customer_Tel) VALUES("C01","Ali",71321009);
+
+INSERT INTO Customer (Customer_id,customer_Name,customer_Tel) VALUES("C02","ASMA",77345823);
+
+INSERT INTO Product (Product_id,product_name,category,Price) VALUES ("P01","Samsung Galaxy 520","Smartphone",3299);
+
+INSERT INTO Product (Product_id,product_name,category,Price) VALUES ("P02","ASUS Notebook","pc",4599);
+
+INSERT INTO Orders  (Customer_id,Product_id,OrderDate,Quality,ToTal_amount) values("C01","P02",0,2,9198);
+
+INSERT INTO Orders  (Customer_id,Product_id,OrderDate,Quality,Total_amount) values ("C02","P01",28/05/2020,1,3299);
